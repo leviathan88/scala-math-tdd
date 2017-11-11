352fe13244910209390fe5bb782451b130507612
Elvis Dedic <elvis26112009@gmail.com>
2017-11-11 14:12:23 +0100

    overriding toString, hashCode and equals and creating add, subtract, multiply and scalarMultiply

--- src/main/scala/LinearAlgebra/MyVector.scala
+++ src/main/scala/LinearAlgebra/MyVector.scala
@@ -5,11 +5,32 @@ package LinearAlgebra
   */
 class MyVector(val x: Double, val y: Double, val z: Double=0) {
 
-  override def toString = if (this.z>0) s"[X: ${this.x}, Y: ${this.y}, Z:${this.z}]" else s"[X: ${this.x}, Y: ${this.y}]"
+  // Private methods
+  private def f(point: Double): Float = point.toFloat
 
-  def add(v1: MyVector) = new MyVector(this.x+v1.x, this.y+v1.y, this.z + v1.z)
+  // Overriden methods
+  override def toString =
+    if (this.z!=0) s"[X: ${f(this.x)}, Y: ${f(this.y)}, Z: ${f(this.z)}]" else s"[X: ${f(this.x)}, Y: ${f(this.y)}]"
+
+  override def hashCode = (x, y, z).##
+
+  override def equals(that: Any) = that match {
+    case that: MyVector => this.x == that.x && this.y == that.y && this.z == that.z
+    case _ => false
+  }
+
+  // public instance methods
+  def add(v1: MyVector) = new MyVector(this.x + v1.x, this.y + v1.y, this.z + v1.z)
+  def subtract(v1: MyVector) = new MyVector(this.x - v1.x, this.y - v1.y, this.z - v1.z)
+  def multiply(v1: MyVector) = new MyVector(this.x * v1.x, this.y * v1.y, this.z * v1.z)
+  def scalarMultiply(scalar: Double) = new MyVector(scalar*this.x, scalar*this.y, scalar*this.z)
 }
 
 object MyVector {
-  def add(v1: MyVector, v2: MyVector) = new MyVector(v1.x+v2.x, v1.y+v2.y, v1.z + v2.z)
+
+  // public static methods
+  def add(v1: MyVector, v2: MyVector) = new MyVector(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z)
+  def subtract(v1: MyVector, v2: MyVector) = new MyVector(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z)
+  def multiply(v1: MyVector, v2: MyVector) = new MyVector(v1.x * v2.x, v1.y * v2.y, v1.z * v2.z)
+  def scalarMultiply(scalar: Double, v: MyVector) = new MyVector(scalar*v.x, scalar*v.y, scalar*v.z)
 }
